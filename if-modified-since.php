da6be158ba18044ec01bfef41ccb9db5fe77ff2d
Ezra Verheijen <everheijen@trimm.nl>
2017-04-10 19:07:26 +0200

    - Removed PHP < 5.4 legacy code - Added prevention for class name collision - Modified the plugin to only output a Last-Modified header if the response is not 304

--- if-modified-since.php
+++ if-modified-since.php
@@ -1,14 +1,12 @@
 <?php
 /**
- * Plugin Name: If Modified Since
- * Plugin URI:  http://wordpress.org/plugins/if-modified-since/
- * Description: Lightweight plugin to handle the `If Modifed Since` functionality.
- * Version:     0.9
+ * Plugin Name: If-Modified-Since
+ * Plugin URI:  https://wordpress.org/plugins/if-modified-since/
+ * Description: Lightweight plugin that handles the If-Modifed-Since HTTP header functionality.
+ * Version:     1.0
  * Author:      Ezra Verheijen
- * Author URI:  http://profiles.wordpress.org/ezraverheijen/
+ * Author URI:  https://profiles.wordpress.org/ezraverheijen/
  * License:     GPL v3
- * 
- * @link https://www.feedthebot.com/ifmodified.html
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,82 +22,77 @@
  * along with this program. If not, see <http://www.gnu.org/licenses>.
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'If_Modified_Since' ) ) :
 
 class If_Modified_Since {
 
 	public function __construct() {
-        add_action( 'template_redirect', array( $this, 'respond_if_modified_since' ) );
+		add_action( 'template_redirect', [ $this, 'respond_if_modified_since' ] );
 	}
 
-    public function respond_if_modified_since() {
-        if ( ! $mtime = $this->get_mtime() ) {
-            return;
-        }
+	public function respond_if_modified_since() {
+		if ( ! $mtime = $this->get_mtime() ) {
+			return;
+		}
 
-        if ( ! $this->is_valid_unix_timestamp( $mtime ) ) {
-            return;
-        }
+		if ( isset( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) &&
+			strtotime( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) >= $mtime )
+		{
+			http_response_code( 304 );
+			exit;
+		} else {
+			header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s', $mtime ) . ' GMT' );
+		}
+	}
 
-        header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s', $mtime ) . ' GMT' );
+	private function get_mtime() {
+		global $wp_query;
 
-        if ( isset( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) &&
-            strtotime( $_SERVER['HTTP_IF_MODIFIED_SINCE'] ) >= $mtime )
-        {
-            if ( function_exists( 'http_response_code' ) ) {
-                http_response_code( 304 );
-            } else {
-               header( $this->get_http_protocol() . ' 304 Not Modified' ); 
-            }
+		$mtime = null;
 
-            exit;
-        }
-    }
+		if ( $wp_query->is_home() ) {
+			$mtime = $this->get_archive_mtime( 'post_type', 'post' );
+		} elseif ( $wp_query->is_single() || $wp_query->is_page() ) {
+			if ( $id = $wp_query->get_queried_object_id() ) {
+				$mtime = $this->get_post_mtime( $id );
+			}
+		} elseif ( $wp_query->is_category() ) {
+			if ( $id = $wp_query->get_queried_object_id() ) {
+				$mtime = $this->get_archive_mtime( 'cat', $id );
+			}
+		} elseif ( $wp_query->is_tag() ) {
+			if ( $id = $wp_query->get_queried_object_id() ) {
+				$mtime = $this->get_archive_mtime( 'tag_id', $id );
+			}
+		} elseif ( $wp_query->is_tax() ) {
+			if ( $id = $wp_query->get_queried_object_id() ) {
+				$mtime = $this->get_archive_mtime( 'tax_id', $id );
+			}
+		} elseif ( $wp_query->is_author() ) {
+			if ( $id = $wp_query->get_queried_object_id() ) {
+				$mtime = $this->get_archive_mtime( 'author', $id );
+			}
+		} elseif ( $wp_query->is_year() ) {
+			$year  = $wp_query->get( 'm' ) ? $wp_query->get( 'm' ) : $wp_query->get( 'year' );
+			$mtime = $this->get_archive_mtime( 'year', $year );
+		} elseif ( $wp_query->is_month() ) {
+			$month = $wp_query->get( 'm' ) ? mb_substr( $wp_query->get( 'm' ), 4, 2 ) : $wp_query->get( 'monthnum' );
+			$mtime = $this->get_archive_mtime( 'monthnum', $month );
+		} elseif ( $wp_query->is_day() ) {
+			$day   = $wp_query->get( 'm' ) ? mb_substr( $wp_query->get( 'm' ), 6, 2 ) : $wp_query->get( 'day' );
+			$mtime = $this->get_archive_mtime( 'day', $day );
+		} elseif ( $wp_query->is_post_type_archive() ) {
+			$mtime = $this->get_archive_mtime( 'post_type', $wp_query->get( 'post_type' ) );
+		}
 
-    public function get_mtime() {
-        global $wp_query;
+		return apply_filters( 'if_modified_since', $mtime );
+	}
 
-        $mtime = null;
-
-        if ( $wp_query->is_home() ) {
-            $mtime = $this->get_archive_mtime( 'post_type', 'post' );
-        } elseif ( $wp_query->is_single() || $wp_query->is_page() ) {
-            if ( $id = $wp_query->get_queried_object_id() ) {
-                $mtime = $this->get_post_mtime( $id );
-            }
-        } elseif ( $wp_query->is_category() ) {
-            if ( $id = $wp_query->get_queried_object_id() ) {
-                $mtime = $this->get_archive_mtime( 'cat', $id );
-            }
-        } elseif ( $wp_query->is_tag() ) {
-            if ( $id = $wp_query->get_queried_object_id() ) {
-                $mtime = $this->get_archive_mtime( 'tag_id', $id );
-            }
-        } elseif ( $wp_query->is_tax() ) {
-            if ( $id = $wp_query->get_queried_object_id() ) {
-                $mtime = $this->get_archive_mtime( 'tax_id', $id );
-            }
-        } elseif ( $wp_query->is_author() ) {
-            if ( $id = $wp_query->get_queried_object_id() ) {
-                $mtime = $this->get_archive_mtime( 'author', $id );
-            }
-        } elseif ( $wp_query->is_year() ) {
-            $year  = $wp_query->get( 'm' ) ? $wp_query->get( 'm' ) : $wp_query->get( 'year' );
-            $mtime = $this->get_archive_mtime( 'year', $year );
-        } elseif ( $wp_query->is_month() ) {
-            $month = $wp_query->get( 'm' ) ? mb_substr( $wp_query->get( 'm' ), 4, 2 ) : $wp_query->get( 'monthnum' );
-            $mtime = $this->get_archive_mtime( 'monthnum', $month );
-        } elseif ( $wp_query->is_day() ) {
-            $day   = $wp_query->get( 'm' ) ? mb_substr( $wp_query->get( 'm' ), 6, 2 ) : $wp_query->get( 'day' );
-            $mtime = $this->get_archive_mtime( 'day', $day );
-        } elseif ( $wp_query->is_post_type_archive() ) {
-            $mtime = $this->get_archive_mtime( 'post_type', $wp_query->get( 'post_type' ) );
-        }
-
-        return apply_filters( 'if_modified_since', $mtime );
-    }
-
-	public function get_archive_mtime( $query, $object_id ) {
+	private function get_archive_mtime( $query, $object_id ) {
 		$mtime = null;
 
 		$query = new WP_Query( $query . '=' . $object_id . '&posts_per_page=1&no_found_rows=true' );
@@ -117,19 +110,12 @@ class If_Modified_Since {
 		return $mtime;
 	}
 
-	public function get_post_mtime( $post_id ) {
+	private function get_post_mtime( $post_id ) {
 		return get_post_modified_time( 'U', false, $post_id );
 	}
 
-    public function is_valid_unix_timestamp( $timestamp ) {
-        return ( is_int( $timestamp ) &&
-                $timestamp === strtotime( date( 'd-m-Y H:i:s', $timestamp ) ) );
-    }
-
-    public function get_http_protocol() {
-        return ( ! empty( $_SERVER['SERVER_PROTOCOL'] ) ) ? sanitize_text_field( $_SERVER['SERVER_PROTOCOL'] ) : 'HTTP/1.1';
-    }
-
 }
+
+endif;
 
 $if_modified_since = new If_Modified_Since();
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
-=== If Modified Since ===
+﻿=== If-Modified-Since ===
 Contributors: ezraverheijen
 Donate link: http://bit.ly/1eC8iDE
-Tags: if modified since, IF Modifeid Since, If-Modified-Since, seo, SEO, search engine optimization, google, bing, yahoo
+Tags: if modified since, If Modified Since, If-Modified-Since, seo, SEO, search engine optimization, google, bing, yahoo
 Requires at least: 3.5
-Tested up to: 4.2
-Stable tag: 0.9
+Tested up to: 4.7.3
+Stable tag: 1.0
 License: GPL v3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -12,14 +12,14 @@ Comply with Webmaster Guidelines, save some bandwith and reduce server load with
 
 == Description ==
 
-If Modified Since tells search engine spiders one of two things about a webpage:
+If-Modified-Since tells search engine spiders one of two things about a webpage:
 
 * This webpage has not changed, no need to download again.
 * This webpage has changed so download again because there is new information.
 
 If the requested page has not been modified since the time specified by a search engine spider, a 304 (not modified) response will be returned without any message-body.
 
-If you have any issues using If Modified Since, find a bug or have an idea to make the plugin even better then please [help to improve If Modified Since](https://github.com/ezraverheijen/wp-if-modified-since).
+If you have any issues using If-Modified-Since, find a bug or have an idea to make the plugin even better then please [help to improve If-Modified-Since](https://github.com/ezraverheijen/wp-if-modified-since).
 If you don’t report it, I can’t fix it!
 
 == Installation ==
@@ -32,15 +32,23 @@ If you don’t report it, I can’t fix it!
 
 = Do I need this when I already use a caching plugin like W3 Total Cache? =
 
-Some plugins like W3TC support the If-Modified-Since header, so there's no need to install this plugin.
+Some plugins like W3TC support the If-Modified-Since HTTP header, so there's no need to install this plugin.
 
 = How do I know if the plugin works after installing? =
 
-You can test If Modified Since [here](https://varvy.com/tools/if-modified/)
+You can test If-Modified-Since [here](https://www.hscripts.com/tools/if-modified-since/)
 
 == Screenshots ==
 
 == Changelog ==
+
+= 1.0 =
+* Stable release
+
+* Enhancements:
+	* Removed PHP < 5.4 legacy code
+	* Added prevention for class name collision
+	* Modified the plugin to only output a Last-Modified header if the response is not 304
 
 = 0.9 =
 * Beta release
